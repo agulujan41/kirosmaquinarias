3ec80c1cbb67bc6d091ecba801ce0427a8feb23e
agulujan41 <agulujan41proj@gmail.com>
2023-12-23 16:13:55 -0300

    navbar 50 %

--- src/components/utils/Navbar.js
+++ src/components/utils/Navbar.js
@@ -1,9 +1,19 @@
 import { Box,Typography } from '@mui/material'
+import {Cursor,useTypewriter} from 'react-simple-typewriter'
+import MenuIcon from '@mui/icons-material/Menu';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import AgricultureIcon from '@mui/icons-material/Agriculture';
 import CallIcon from '@mui/icons-material/Call';
 import React from 'react'
 
 const Navbar = () => {
+
+  const [detailsTitle] =useTypewriter({
+    words:['maquinarias'],
+    loop:{},
+    typeSpeed:100,
+    deleteSpeed:25
+  })
   return (
     <>
         <Box sx = {{
@@ -22,7 +32,8 @@ const Navbar = () => {
         top:0,
         height:'90px',
         '@media (max-width:1000px)':{
-            height:'60px'
+            height:'60px',
+            pl:2
         }
 
       }} id="navbar" >
@@ -40,18 +51,27 @@ const Navbar = () => {
               gap:'15px',
               height:'60px',
               alignItems: 'center',
+              '@media (max-width:1000px)':{
+                gap:'7px'
+            }
               
             }}>
               <Typography sx={{fontFamily:"'Manrope', sans-serif" ,fontSize:'3.2em',fontWeight:'bold',fontStyle:'italic',alignSelf: 'bottom'
-              ,'@media (max-width:1000px)':{
-                fontSize:'2.0em'
+              ,zIndex:150,'@media (max-width:1000px)':{
+                fontSize:'1.5em'
+
             }
-            }}>KYRO</Typography>
+            }}>KIRO</Typography>
+              
               <Typography  sx={{fontFamily:"'Manrope', sans-serif" ,fontSize:'2.3em',fontStyle:'italic',fontWeight:'bold',alignSelf: 'bottom'
               ,'@media (max-width:1000px)':{
-                fontSize:'1.7em'
+                fontSize:'1.3em'
             }
-            }}>maquinarias</Typography>
+            }}>{detailsTitle}</Typography>
+            <Cursor cursorStyle=''>
+            
+            </Cursor>
+            <Box component='img' src='./maquinarias/logotractor.png' sx={{height:'45px'}}/>
             </Box>
           </Box>
           
@@ -74,6 +94,10 @@ const Navbar = () => {
               </Box>
             
           </Box>
+          <MenuIcon sx={{display:'none','@media (max-width:1000px)':{
+              display:'flex',mr:2
+          }}}/>
+
       </Box>
     </>
   )
